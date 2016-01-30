51c8d44e186b77c651e7f035e251c302529a8b31
root <root@debian>
2016-01-30 12:27:24 -0500

    added capturing of fifo stats

--- oslab3/kernel_loging.sh
+++ oslab3/kernel_loging.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+sudo rm -f fifo_stats
+sudo touch fifo_stats
 while [[ true ]]; do
 	dmesg >> ./kernel_log
 	sudo dmesg -C
+	printf "$(cat /proc/deeds_fifo_stats)\n" >> fifo_stats
 	sleep 2
 done
